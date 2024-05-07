b235a95141537f09a30e9387001889b7c0605873
Energy Ikeda <135154944+camen89@users.noreply.github.com>
2024-05-08 02:59:32 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
+$(document).ready(function () {
+    let headerHeight = $('.Header_img').height();
+    $('.Icon_img').css('top', headerHeight / 2 + 'px');
+    $('.Header_group').css('margin-bottom', headerHeight / 2 + 'px');
+
+    let iconImgWidth = $('.Timeline_img').width();
+    $('.content_img1').css('margin-left', iconImgWidth + iconImgWidth / 2 + 'px');
+    console.log(headerHeight, iconImgWidth);
+
+    $(window).resize(function () {
+        let headerHeight = $('.Header_img').height();
+        $('.Icon_img').css('top', headerHeight / 2 + 'px');
+        $('.Header_group').css('margin-bottom', headerHeight / 2 + 'px');
+
+        let iconImgWidth = $('.Timeline_img').width();
+        $('.content_img1').css('margin-left', iconImgWidth + iconImgWidth / 2 +'px');
+        console.log(headerHeight, iconImgWidth);
+    })
+});
+
+
 
